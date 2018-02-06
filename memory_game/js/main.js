11d5fbbff9fce87d3f33a2a8bcf82c7d57a14f05
DontForgetTheH <seraveza@gmail.com>
2018-02-05 17:35:25 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,7 +21,6 @@ var cards = [
 	}
 	];
 var cardsInPlay = [];
-
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -30,16 +29,30 @@ var checkForMatch = function () {
 			alert("Sorry, try again.");
 		}
 	}
-}
+};
 
-var flipCard = function (cardId) {
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log('User flipped ' + cards[cardId].cardImage);
 	console.log('User flipped ' + cards[cardId].suit);
-checkForMatch();
+	checkForMatch();
+	
+};
+
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
 }
 
-//"Calling" the flipcard function"
-flipCard(0);
-flipCard(2);
+
+//"Calling" the createBoard function
+createBoard();
